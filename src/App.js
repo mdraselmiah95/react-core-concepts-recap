99394461ffe89b3c9ed6f827a9582aa916e9ec4e
rasel <russelroy15@gmail.com>
2021-09-20 15:30:57 +0600

    WIP on React core concept recap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+      <MyComponent></MyComponent>
+    </div>
+  );
 }
 
 function MyComponent() {
   return (
     <div>
       <h1>This is a start of new React concept recap</h1>
+      <p>I can write my own component</p>
     </div>
   );
 }
