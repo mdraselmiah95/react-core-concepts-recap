3eec180083f5c342c24ff9082403f5f11f457c5f
rasel <russelroy15@gmail.com>
2021-09-20 15:50:52 +0600

    WIP on React core concept recap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -15,13 +16,14 @@ const handleAddPoint = () => {
 };
 
 function MyComponent(props) {
+  const [points, setPoints] = useState(0);
   return (
     <div className="Recap">
       <h1>React concept recap</h1>
       <p>I can write my own component</p>
       <h1>This is a cool Brand: {props.brand}</h1>
       <p style={{ color: "pink", fontWeight: "bold" }}>
-        Sorry for the Price: {props.price}
+        Sorry for the Price: {props.price}. My points: {points}
       </p>
       <button onClick={handleAddPoint}>Add Point</button>
     </div>
